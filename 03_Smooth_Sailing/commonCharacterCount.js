9eff41c5872a8e5adf262b0e2e23d77c8f58a789
Taylor O'Reilly <taylor@taylororeilly.dev>
2022-07-09 16:18:16 -0400

    Refactor commonCharacterCount

--- 03_Smooth_Sailing/commonCharacterCount.js
+++ 03_Smooth_Sailing/commonCharacterCount.js
@@ -1,69 +1,41 @@
 // Given two strings, find the number of common characters between them.
 
-//     Example
+// Example
 
 // For s1 = "aabcc" and s2 = "adcaa", the output should be
-// commonCharacterCount(s1, s2) = 3.
+// solution(s1, s2) = 3.
 
 // Strings have 3 common characters - 2 "a"s and 1 "c".
 
-//     Input / Output
+// Input/Output
 
-//     [execution time limit]4 seconds(js)
+//     [execution time limit] 4 seconds (js)
 
 //     [input] string s1
 
-// A string consisting of lowercase English letters.
+//     A string consisting of lowercase English letters.
 
-// Guaranteed constraints:
-// 1 ≤ s1.length ≤ 15.
+//     Guaranteed constraints:
+//     1 ≤ s1.length < 15.
 
-// [input] string s2
+//     [input] string s2
 
-// A string consisting of lowercase English letters.
+//     A string consisting of lowercase English letters.
 
-// Guaranteed constraints:
-// 1 ≤ s2.length ≤ 15.
+//     Guaranteed constraints:
+//     1 ≤ s2.length < 15.
 
-// [output] integer
+//     [output] integer
 
-// [JavaScript(ES6)] Syntax Tips
 
-// // Prints help message to the console
-// // Returns a string
-// function helloWorld(name) {
-//     console.log("This prints to the console when you Run Tests");
-//     return "Hello, " + name;
-// }
-
-function commonCharacterCount(s1, s2) {
-  arr1 = s1.split("");
-  arr2 = s2.split("");
-  var common = 0;
-  var largeArr = [];
-  var smallArr = [];
-  //double check we have the corrent order
-  if (arr1.length > arr2.length) {
-    largeArr = arr1;
-    smallArr = arr2;
-  } else {
-    largeArr = arr2;
-    smallArr = arr1;
-  }
-
-  largeArr.forEach(function(char) {
-    for (var i = 0; i < smallArr.length; i++) {
-      if (char == smallArr[i]) {
-        common++;
-        smallArr.splice(i, 1);
-        return;
-      } else if (smallArr.length == 0) {
-        return;
-      }
+function solution(s1, s2) {
+    let count = 0;
+    for(let i = 0; i < s1.length; i++) {
+        let j = s2.indexOf(s1[i]);
+        if(j >= 0){
+            count++;
+            s2 = s2.replace(s1[i],'');
+        }
     }
-  });
-
-  console.log(smallArr);
-  console.log(common);
-  return common;
+    return count;
 }
