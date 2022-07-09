1b6d2aab99826110aa1dabce9c80b27bc942de1b
Taylor O'Reilly <taylor@taylororeilly.dev>
2022-07-09 17:47:34 -0400

    Update reverseParentheses.js

--- 03_Smooth_Sailing/reverseParentheses.js
+++ 03_Smooth_Sailing/reverseParentheses.js
@@ -20,20 +20,17 @@
 
 // [output] string
 
-function reverseParentheses(s) {
-  if (s.includes("(")) {
-    return reverseParentheses(reverseOnce(s));
-  } else {
-    return s;
-  }
+function solution(s) {
+    if (s.includes('(')){
+        return solution(reverseOnce(s));
+    } else {     
+        return s;
+    }
 }
 
-function reverseOnce(s) {
-  var regexp = /\(([^()]*)\)/i;
-  var subStr = regexp.exec(s)[1];
-  subStr = subStr
-    .split("")
-    .reverse()
-    .join("");
-  return s.replace(regexp, subStr);
+function reverseOnce(s){
+    var regexp = /\(([^()]*)\)/i;
+    var subStr = regexp.exec(s)[1];
+    subStr = subStr.split('').reverse().join('');
+    return s.replace(regexp, subStr)
 }
