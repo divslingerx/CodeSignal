d5b1d7a8267ce77010d3f6c45e135f87efdacf1e
Taylor O'Reilly <taylor@taylororeilly.dev>
2022-07-09 21:56:26 -0400

    Refactor knapsackLight

--- 09_Dark_Wilderness/knapsackLight.js
+++ 09_Dark_Wilderness/knapsackLight.js
@@ -50,12 +50,12 @@
 
 // [output] integer
 
-function knapsackLight(value1, weight1, value2, weight2, maxW) {
-  if (weight1 + weight2 <= maxW) return value1 + value2;
-  if (weight1 > maxW && weight2 > maxW) return 0;
-  if (weight1 > maxW) return value2;
-  if (weight2 > maxW) return value1;
-  if (weight1 && weight2 <= maxW) return Math.max(value1, value2);
+function solution(value1, weight1, value2, weight2, maxW) {
+    return Math.max(
+        maxW >= weight1 && value1,
+        maxW >= weight2 && value2,
+        maxW >= weight1 + weight2 && value1 + value2
+    );
 }
 
 // Input:
